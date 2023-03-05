78f55ca4a52deb7edca0edd7adad2266bc1a2a3d
Damitha Perera <damithaperera777@gmail.com>
2023-03-05 19:55:53 +0530

    joi installed

--- module/hotel/hotel.controller.ts
+++ module/hotel/hotel.controller.ts
@@ -6,7 +6,7 @@ import {ErrorResponse, SuccessResponse} from "../../util/responseHandler";
 export const getAllHotelsController = async (req: Request, res: Response) => {
     try {
         const {checkIn, checkOut} = req.query
-        const data = await getAllHotelsService(checkIn, checkOut)
+        const data = await getAllHotelsService(<string>checkIn, <string>checkOut)
         return SuccessResponse(res, data, 'Hotel Details', 200);
     } catch (error) {
         return ErrorResponse(res, {message: error}, 500);
