7ee7370ba8414dae151d8ce28524513b4950a47e
Damitha Perera <damithaperera777@gmail.com>
2023-03-06 14:51:26 +0530

    create hotel joi validation

--- module/hotel/hotel.router.ts
+++ module/hotel/hotel.router.ts
@@ -1,13 +1,13 @@
 import {Router} from 'express';
 import {creatHotelBookingController, getAllHotelsController} from './hotel.controller';
-import {getAllHotelsValidation} from "./hotel.validation";
+import {creatHotelBookingValidation, getAllHotelsValidation} from "./hotel.validation";
 
 
 const routes = Router();
 
 
 routes.get('/all', getAllHotelsValidation, getAllHotelsController);
-routes.post('/booking', creatHotelBookingController);
+routes.post('/booking', creatHotelBookingValidation, creatHotelBookingController);
 
 
 export default routes;
--- module/hotel/hotel.validation.ts
+++ module/hotel/hotel.validation.ts
@@ -19,3 +19,28 @@ export const getAllHotelsValidation = (req: Request, res: Response, next: NextFu
     }
 
 };
+
+export const creatHotelBookingValidation = (req: Request, res: Response, next: NextFunction) => {
+
+    const schema = Joi.object({
+        HotelId: Joi.number().required(),
+        RoomId: Joi.number().required(),
+        CheckIn: Joi.date().format('YYYY-MM-DD').greater('now').required(),
+        CheckOut: Joi.date().format('YYYY-MM-DD').greater(Joi.ref('CheckIn')).required(),
+        CustomerDetails: Joi.object({
+            FirstName: Joi.string().required(),
+            LastName: Joi.string().required(),
+            Email: Joi.string().email().required(),
+            PhoneNumber: Joi.string().required(),
+            Payment: Joi.string().valid('SUCCESS', 'FAILURE').required()
+        }).required()
+    });
+
+    const result = schema.validate(req.body);
+    if (result.error) {
+        return ErrorResponse(res, {message: result.error.message}, 422);
+    } else {
+        next()
+    }
+
+};
