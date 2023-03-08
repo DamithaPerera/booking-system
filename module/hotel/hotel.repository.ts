aa74c5685f8b559647d311ddbe83b9d9de840de9
Damitha Perera <damithaperera777@gmail.com>
2023-03-08 08:24:15 +0530

    add comments to hotel repo

--- module/hotel/hotel.repository.ts
+++ module/hotel/hotel.repository.ts
@@ -1,47 +1,83 @@
-import hotelData from '../../data/hotel.data.json';
-import {Booking} from "../../interface/Booking";
-import path from 'path';
-import fs from 'fs';
+import hotelData from "../../data/hotel.data.json";
+import { Booking } from "../../interface/Booking";
+import path from "path";
+import fs from "fs";
 import cache from "../../util/cache";
 
-const bookingsPath = path.join(__dirname, '..', '..', 'data', 'booking.data.json');
+const bookingsPath = path.join(__dirname, "..", "..", "data", "booking.data.json");
 
-
+/**
+ * Returns all hotels data.
+ */
 export const getAllHotelsRepo = async () => {
-    return hotelData
+  // Return the hotelData array.
+  return hotelData;
 };
 
+/**
+ * Returns all booking data by reading from a JSON file.
+ */
 export const getAllBookingRepo = async () => {
-    const bookingsJSON = fs.readFileSync(bookingsPath, 'utf-8');
-    return JSON.parse(bookingsJSON)
+  // Read the bookings data from the JSON file.
+  const bookingsJSON = fs.readFileSync(bookingsPath, "utf-8");
+  // Convert the JSON string to an array of objects and return it
+  return JSON.parse(bookingsJSON);
 };
 
-
+/**
+ * Creates a new booking by adding it to the bookings data array and writing the
+ * updated data to a JSON file.
+ * @param requestBody
+ */
 export const createBookingRepo = async (requestBody: Booking) => {
-    const bookingsJSON = fs.readFileSync(bookingsPath, 'utf-8');
-    const bookings = JSON.parse(bookingsJSON);
-    bookings.Booking.push(requestBody);
+  // Read the bookings data from the JSON file.
+  const bookingsJSON = fs.readFileSync(bookingsPath, "utf-8");
+  // Convert the JSON string to an array of objects.
+  const bookings = JSON.parse(bookingsJSON);
+  // Add the new booking to the bookings data array.
+  bookings.Booking.push(requestBody);
 
-    const updatedJSON = JSON.stringify(bookings, null, 2);
-
-    fs.writeFileSync(bookingsPath, updatedJSON);
+  // Convert the updated bookings data to a JSON string.
+  const updatedJSON = JSON.stringify(bookings, null, 2);
+  // Write the updated bookings data to the JSON file.
+  fs.writeFileSync(bookingsPath, updatedJSON);
 };
 
+/**
+ * Reads and returns the booking data from a JSON file.
+ */
 export const getBookingRepo = async () => {
-    const bookingsJSON = fs.readFileSync(bookingsPath, 'utf-8');
-    return JSON.parse(bookingsJSON)
+  // Read the bookings data from the JSON file.
+  const bookingsJSON = fs.readFileSync(bookingsPath, "utf-8");
+  // Convert the JSON string to an array of objects and return it.
+  return JSON.parse(bookingsJSON);
 };
 
+/**
+ * Updates the booking data by writing the updated data to a JSON file and updating the cache.
+ * @param updatedJSON
+ */
 export const updateBookingRepo = async (updatedJSON: string | NodeJS.ArrayBufferView) => {
-    cache.del("getBookings");
-    cache.set("getBookings", updatedJSON);
-    return fs.writeFileSync(bookingsPath, updatedJSON);
+  // Remove the old data from the cache.
+  cache.del("getBookings");
+  // Add the updated data to the cache.
+  cache.set("getBookings", updatedJSON);
+  // Write the updated data to the JSON file.
+  return fs.writeFileSync(bookingsPath, updatedJSON);
 };
 
+/**
+ * Cancels a booking by updating the booking data and writing the updated data
+ * to a JSON file and the cache.
+ * @param bookingData
+ */
 export const cancelBookingRepo = async (bookingData: any) => {
-    cache.del("getBookings");
-    const updatedJSON = JSON.stringify(bookingData, null, 2);
-    cache.set("getBookings", updatedJSON);
-    return fs.writeFileSync(bookingsPath, updatedJSON);
-
+  // Remove the old data from the cache.
+  cache.del("getBookings");
+  // Convert the updated data to a JSON string.
+  const updatedJSON = JSON.stringify(bookingData, null, 2);
+  // Add the updated data to the cache.
+  cache.set("getBookings", updatedJSON);
+  // Write the updated data to the JSON file.
+  return fs.writeFileSync(bookingsPath, updatedJSON);
 };
