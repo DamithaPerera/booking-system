0081481ef908cd35724ba18ec219eedaddc504dd
Damitha Perera <damithaperera777@gmail.com>
2023-03-08 08:43:23 +0530

    comments

--- util/responseHandler.ts
+++ util/responseHandler.ts
@@ -1,9 +1,13 @@
-import { Response } from 'express';
+import { Response } from "express";
 
+// This function is used to send error responses back to the client with the specified error message and status code.
+// It takes in the response object, error object, and status code as parameters, and returns the updated response object.
 export const ErrorResponse = (res: Response, error: any, code = 400): Response => {
     return res.status(code).json({ success: false, data: { errorMessage: error.message } });
 };
 
+// This function is used to send success responses back to the client with the specified data, message, and status code.
+// It takes in the response object, data object, message string, and status code as parameters, and returns the updated response object.
 export const SuccessResponse = (res: Response, data: any, message: string, code = 200): Response => {
     return res.status(code).json({ success: true, data, message });
 };
