d728eaf99f6750ecb13d3e2edfdac6a6cb55f68c
Damitha Perera <damithaperera777@gmail.com>
2023-03-08 08:47:01 +0530

    add comments

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
-import express, {Express, NextFunction, Request, Response} from 'express';
+import express, {Express, NextFunction, Request, Response} from "express";
 import bodyParser from "body-parser";
-import dotenv from 'dotenv';
-import hotelRouter from './module/hotel/hotel.router';
+import dotenv from "dotenv";
+import hotelRouter from "./module/hotel/hotel.router";
 import {authRequest} from "./util/auth";
 
-
+// Load environment variables from .env file
 dotenv.config();
-
+// Create Express app instance
 const app: Express = express();
+// Set port number
 const port = process.env.PORT || 3000;
-
+// Use body-parser middleware to parse request body
 app.use(bodyParser.json());
 
 // Authenticate all requests
@@ -17,13 +18,10 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     authRequest(req, res, next);
 });
 
-app.use('/v1/hotels', hotelRouter)
+// Register hotel router
+app.use("/v1/hotels", hotelRouter)
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
-
+// Start server and listen on specified port
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
