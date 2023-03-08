1bd2da330f5aa974e56a421b0d379f1986f2aa56
Damitha Perera <damithaperera777@gmail.com>
2023-03-08 08:10:35 +0530

    add comments hotel service

--- module/hotel/hotel.service.ts
+++ module/hotel/hotel.service.ts
@@ -1,143 +1,218 @@
 import {
-    cancelBookingRepo,
-    createBookingRepo,
-    getAllBookingRepo,
-    getAllHotelsRepo,
-    updateBookingRepo
+  cancelBookingRepo,
+  createBookingRepo,
+  getAllBookingRepo,
+  getAllHotelsRepo,
+  updateBookingRepo
 } from "./hotel.repository";
-import {Booking} from "../../interface/Booking";
-import {Hotel} from "../../interface/hotel";
-import {BookingData} from "../../interface/bookingData";
-import {v4 as uuidv4} from 'uuid';
+import { Booking } from "../../interface/Booking";
+import { Hotel } from "../../interface/hotel";
+import { BookingData } from "../../interface/bookingData";
+import { v4 as uuidv4 } from "uuid";
 import cache from "../../util/cache";
 
-
+/**
+ * This function retrieves a list of available hotels for a given date range and
+ * pagination parameters.
+ * @param checkIn
+ * @param checkOut
+ * @param page
+ * @param perPage
+ */
 export const getAllHotelsService = async (checkIn: string, checkOut: string, page: number, perPage: number) => {
-    const { Booking, Hotels } = await getCacheForBookingsAndHotels();
+  // Retrieve cached data for Bookings and Hotels
+  const { Booking, Hotels } = await getCacheForBookingsAndHotels();
 
-    // Define the date range to check availability
-    const checkInDate = new Date(checkIn);
-    const checkOutDate = new Date(checkOut);
+  // Define the date range to check availability based on the check-in and check-out dates
+  const checkInDate = new Date(checkIn);
+  const checkOutDate = new Date(checkOut);
 
-    // Find all booked room IDs within the date range
-    const bookedRoomIds = Booking.filter((booking) => {
-        const bookingCheckInDate = new Date(booking.CheckIn);
-        const bookingCheckOutDate = new Date(booking.CheckOut);
-        return bookingCheckInDate < checkOutDate && bookingCheckOutDate > checkInDate;
-    }).map((booking) => booking.RoomId);
+  // Find all booked room IDs within the date range
+  const bookedRoomIds = Booking.filter((booking) => {
+    const bookingCheckInDate = new Date(booking.CheckIn);
+    const bookingCheckOutDate = new Date(booking.CheckOut);
+    // Filter bookings where there is a conflict with the requested date range
+    return bookingCheckInDate < checkOutDate && bookingCheckOutDate > checkInDate;
+  }).map((booking) => booking.RoomId);
 
-    // Filter out all rooms that are already booked within the date range
-    const availableHotels = Hotels.map((hotel) => ({
-        ...hotel,
-        Rooms: hotel.Rooms.filter((room) => !bookedRoomIds.includes(room.RoomId)),
-    }));
+  // Filter out all rooms that are already booked within the requested date range
+  const availableHotels = Hotels.map((hotel) => ({
+    ...hotel,
+    Rooms: hotel.Rooms.filter((room) => !bookedRoomIds.includes(room.RoomId))
+  }));
 
-    // Calculate the start and end indexes for the current page
-    const startIndex = (page - 1) * perPage;
-    const endIndex = startIndex + perPage;
+  // Calculate the start and end indexes for the current page
+  const startIndex = (page - 1) * perPage;
+  const endIndex = startIndex + perPage;
 
-    // Return the hotels for the current page
-    return availableHotels.slice(startIndex, endIndex);
+  // Return the hotels for the current page, based on the calculated start and end indexes
+  return availableHotels.slice(startIndex, endIndex);
 };
 
+/**
+ * Create Hotel Booking Service
+ * @param requestBody
+ * This function creates a new hotel booking by generating a unique BookingId,
+ * validating the request against cached data for Bookings and Hotels, and
+ * then creating the new booking in the database via createBookingRepo.
+ */
 export const creatHotelBookingService = async (requestBody: Booking) => {
-    const {Booking, Hotels} = await getCacheForBookingsAndHotels();
-    validateBooking(requestBody, Hotels, Booking);
-    requestBody.BookingId = uuidv4();
-    await createBookingRepo(requestBody);
-    return {
-        "Booking Id": requestBody.BookingId
-    }
-}
+  // Retrieve cached data for Bookings and Hotels
+  const { Booking, Hotels } = await getCacheForBookingsAndHotels();
 
+  // Validate the booking request using cached data for Hotels and existing Bookings
+  validateBooking(requestBody, Hotels, Booking);
+
+  // Generate a new unique BookingId using the uuidv4 library
+  requestBody.BookingId = uuidv4();
+
+  // Create the new booking in the database via the createBookingRepo function
+  await createBookingRepo(requestBody);
+
+  // Return the newly created BookingId as a success message
+  return {
+    "Booking Id": requestBody.BookingId
+  };
+};
+
+/**
+ * This function validates a booking request by checking if the hotel and room exist,
+ * if the room is available during the requested date range, and if the date range is valid.
+ * @param booking
+ * @param hotels
+ * @param bookings
+ */
 function validateBooking(booking: Booking, hotels: Hotel[], bookings: Booking[]) {
-    const hotel = hotels.find(h => h.HotelId === booking.HotelId);
-    if (!hotel) {
-        throw new Error(`Error: Hotel with ID ${booking.HotelId} does not exist.`);
-    }
+  // Check if the hotel specified in the booking request exists
+  const hotel = hotels.find(h => h.HotelId === booking.HotelId);
+  if (!hotel) {
+    throw new Error(`Error: Hotel with ID ${booking.HotelId} does not exist.`);
+  }
 
-    const room = hotel.Rooms.find(r => r.RoomId === booking.RoomId);
-    if (!room) {
-        throw new Error(`Error: Room with ID ${booking.RoomId} does not exist in hotel ${booking.HotelId}.`);
-    }
+  // Check if the room specified in the booking request exists within the specified hotel
+  const room = hotel.Rooms.find(r => r.RoomId === booking.RoomId);
+  if (!room) {
+    throw new Error(`Error: Room with ID ${booking.RoomId} does not exist in hotel ${booking.HotelId}.`);
+  }
 
-    const conflictingBooking = bookings.find(b =>
-        b.HotelId === booking.HotelId &&
-        b.RoomId === booking.RoomId &&
-        ((new Date(b.CheckIn) >= new Date(booking.CheckIn) && new Date(b.CheckIn) < new Date(booking.CheckOut)) ||
-            (new Date(b.CheckOut) > new Date(booking.CheckIn) && new Date(b.CheckOut) <= new Date(booking.CheckOut)))
-    );
-    if (conflictingBooking) {
-        throw new Error(`Error: Room with ID ${booking.RoomId} in hotel ${booking.HotelId} is not available during the specified date range.`);
-    }
+  // Check if the requested room is available during the requested date range
+  const conflictingBooking = bookings.find(b =>
+    b.HotelId === booking.HotelId &&
+    b.RoomId === booking.RoomId &&
+    ((new Date(b.CheckIn) >= new Date(booking.CheckIn) && new Date(b.CheckIn) < new Date(booking.CheckOut)) ||
+      (new Date(b.CheckOut) > new Date(booking.CheckIn) && new Date(b.CheckOut) <= new Date(booking.CheckOut)))
+  );
+  if (conflictingBooking) {
+    throw new Error(`Error: Room with ID ${booking.RoomId} in hotel ${booking.HotelId} is not available during the specified date range.`);
+  }
 
-    const checkInDate = new Date(booking.CheckIn);
-    const checkOutDate = new Date(booking.CheckOut);
-    if (isNaN(checkInDate.getTime()) || isNaN(checkOutDate.getTime()) || checkInDate >= checkOutDate) {
-        throw new Error(`Error: Invalid date range specified.`);
-    }
+  // Check if the specified date range is valid
+  const checkInDate = new Date(booking.CheckIn);
+  const checkOutDate = new Date(booking.CheckOut);
+  if (isNaN(checkInDate.getTime()) || isNaN(checkOutDate.getTime()) || checkInDate >= checkOutDate) {
+    throw new Error(`Error: Invalid date range specified.`);
+  }
 }
 
+/**
+ * Update an existing hotel booking
+ * @param requestBody
+ * @param hotelId
+ * @param roomId
+ * @param bookingId
+ */
 export const updateHotelBookingService = async (requestBody: Booking, hotelId: number, roomId: number, bookingId: string) => {
-    const bookings: BookingData = await getCacheForBookingsAndHotels();
-    // Find the index of the booking to update based on HotelId and RoomId
-    const bookingIndex = bookings.Booking.findIndex((b: Booking) => b.HotelId === hotelId && b.RoomId === roomId && b.BookingId === bookingId);
+  // Get the cached booking and hotel data
+  const bookings: BookingData = await getCacheForBookingsAndHotels();
+  // Find the index of the booking to update based on HotelId, RoomId, and BookingId
+  const bookingIndex = bookings.Booking.findIndex((b: Booking) => b.HotelId === hotelId && b.RoomId === roomId && b.BookingId === bookingId);
 
-    if (bookingIndex === -1) {
-        throw new Error(`Booking not found for hotelId ${hotelId}, roomId ${roomId} and bookingId ${bookingId}`);
-    }
+  // Throw an error if the booking to update is not found
+  if (bookingIndex === -1) {
+    throw new Error(`Booking not found for hotelId ${hotelId}, roomId ${roomId} and bookingId ${bookingId}`);
+  }
 
-    // Update the fields of the booking at the found index
-    bookings.Booking[bookingIndex].CheckIn = requestBody.CheckIn;
-    bookings.Booking[bookingIndex].CheckOut = requestBody.CheckOut;
-    bookings.Booking[bookingIndex].CustomerDetails = requestBody.CustomerDetails;
+  // Update the fields of the booking at the found index
+  bookings.Booking[bookingIndex].CheckIn = requestBody.CheckIn;
+  bookings.Booking[bookingIndex].CheckOut = requestBody.CheckOut;
+  bookings.Booking[bookingIndex].CustomerDetails = requestBody.CustomerDetails;
 
-    delete bookings.Hotels
+  // Remove the Hotels property from the cached data (not needed for updating bookings)
+  delete bookings.Hotels;
 
-    // Write the updated JSON back to the file
-    const updatedJSON = JSON.stringify(bookings, null, 2);
-    await updateBookingRepo(updatedJSON);
+  // Write the updated JSON back to the file
+  const updatedJSON = JSON.stringify(bookings, null, 2);
+  await updateBookingRepo(updatedJSON);
 
-    return bookings.Booking[bookingIndex];
-}
+  // Return the updated booking information
+  return bookings.Booking[bookingIndex];
+};
 
 
-
+/**
+ * cancelHotelBookingService
+ * @param hotelId
+ * @param roomId
+ * @param bookingId
+ */
 export const cancelHotelBookingService = async (hotelId: number, roomId: number, bookingId: string) => {
-    const bookingData: BookingData = await getCacheForBookingsAndHotels();
-    const index = bookingData.Booking.findIndex(
-        (booking: Booking) => booking.HotelId === hotelId && booking.RoomId === roomId && booking.BookingId === bookingId
-    );
+  //get the booking data from cache
+  const bookingData: BookingData = await getCacheForBookingsAndHotels();
 
-    if (index === -1) {
-        throw new Error(`Booking not found for hotelId ${hotelId}, roomId ${roomId} and bookingId ${bookingId}`);
-    }
+  //the `Booking` array inside the `bookingData` object to find the index of the
+  // booking object that matches the specified `hotelId`, `roomId`, and `bookingId`.
+  const index = bookingData.Booking.findIndex(
+    (booking: Booking) => booking.HotelId === hotelId && booking.RoomId === roomId && booking.BookingId === bookingId
+  );
 
-    delete bookingData.Hotels;
-    bookingData.Booking.splice(index, 1);
-    await cancelBookingRepo(bookingData)
-}
+  // If the `findIndex()` method returns `-1`, it means that no booking object was found that matches the specified
+  // `hotelId`, `roomId`, and `bookingId`. In that case, an error is thrown.
+  if (index === -1) {
+    throw new Error(`Booking not found for hotelId ${hotelId}, roomId ${roomId} and bookingId ${bookingId}`);
+  }
+
+  // The `Hotels` property is deleted from the `bookingData` object to remove any hotel data that might have been cached.
+  delete bookingData.Hotels;
+
+  // The `splice()` method is called on the `Booking` array inside the `bookingData` object to remove the booking object
+  // that matches the specified `hotelId`, `roomId`, and `bookingId`
+  bookingData.Booking.splice(index, 1);
+
+  // update the booking data in the database with the modified `bookingData` object.
+  await cancelBookingRepo(bookingData);
+};
 
 
+/**
+ *This function is used to get the cached data for bookings and hotels. If there
+ * is no cached data, it will fetch data from the repository and store it in cache.
+ */
 export const getCacheForBookingsAndHotels = async () => {
-    const getHotelCache = cache.get<Hotel[]>("getHotels");
-    const getBookingCache = cache.get<Booking[]>("getBookings");
+  // Get the cached data for hotels and bookings
+  const getHotelCache = cache.get<Hotel[]>("getHotels");
+  const getBookingCache = cache.get<Booking[]>("getBookings");
 
-    let Booking: Booking[] = getBookingCache ?? [];
-    let Hotels: Hotel[] = getHotelCache ?? [];
+  // Set the cached data to empty arrays if there is no cached data available
+  let Booking: Booking[] = getBookingCache ?? [];
+  let Hotels: Hotel[] = getHotelCache ?? [];
 
-    if (!getHotelCache || !getBookingCache) {
-        const [bookingData, hotelsData] = await Promise.all([
-            getAllBookingRepo(),
-            getAllHotelsRepo(),
-        ]);
+  // If there is no cached data available, fetch data from the repository and store it in cache
+  if (!getHotelCache || !getBookingCache) {
+    // Fetch data for bookings and hotels from the repository
+    const [bookingData, hotelsData] = await Promise.all([
+      getAllBookingRepo(),
+      getAllHotelsRepo()
+    ]);
 
-        Booking = bookingData.Booking;
-        Hotels = hotelsData.Hotels;
+    // Set the fetched data to variables
+    Booking = bookingData.Booking;
+    Hotels = hotelsData.Hotels;
 
-        cache.set("getBookings", Booking);
-        cache.set("getHotels", Hotels);
-    }
+    // Store the fetched data in cache
+    cache.set("getBookings", Booking);
+    cache.set("getHotels", Hotels);
+  }
 
-    return {Booking, Hotels};
+  // Return the data for bookings and hotels
+  return { Booking, Hotels };
 };
