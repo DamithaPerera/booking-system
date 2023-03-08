57f28ef892ced50996e9a5173a250ac9add0cb7a
Damitha Perera <damithaperera777@gmail.com>
2023-03-08 08:41:34 +0530

    comments

--- util/auth.ts
+++ util/auth.ts
@@ -1,7 +1,7 @@
-import {NextFunction, Request, Response} from 'express';
+import {NextFunction, Request, Response} from "express";
 import {token} from "../util/token.json";
 import {ErrorResponse} from "./responseHandler";
-import {HttpStatusCode} from './statusCode';
+import {HttpStatusCode} from "./statusCode";
 
 /**
  * Simple Authentication
@@ -11,10 +11,13 @@ import {HttpStatusCode} from './statusCode';
  * @Description Checks the JWT token is in the header or not
  */
 export const authRequest = async (req: Request, res: Response, next: NextFunction) => {
-
+    // Extract the token from the authorization header and remove the "Bearer" prefix
     const authHeader = req.headers["authorization"]?.replace("Bearer", "").trim();
+    // Check if the token is missing or doesn't match the expected token
     if (!authHeader || authHeader !== token) {
-        return ErrorResponse(res, {message: authHeader ? 'Invalid Token' : 'Missing Token'}, HttpStatusCode.FORBIDDEN);
+        // If the token is missing or invalid, return an error response with the appropriate message and HTTP status code
+        return ErrorResponse(res, {message: authHeader ? "Invalid Token" : "Missing Token"}, HttpStatusCode.FORBIDDEN);
     }
+    // If the token is valid, call the next middleware function
     next();
 }
